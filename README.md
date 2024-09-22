b0528902d930686fc0ad920301ea56482d3355ab
Allenreinoso28 <Allen.j.reinoso@gmail.com>
2024-09-22 01:17:33 -0400

    REALLY CLEANED IT UP
    created classes to remove redundancy and moved them to their own files.

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
-# VENV
+# ################## VENV ################## #
+
 win       - $ myenv/Scripts/activate
 mac       - $ source/bin/activate
 
 start     - $ python app
 
-# control-system-task
+# ################## control-system-task ################## #
+
 Software for my 2024 Robotics team application. This python program take PS4 controller input sending out data packets to be used to control the robot.
 
-Controll Layout
+Controll Layout 🙃
 
-# Exit
+# ################## Exit ################## #
+
 PS button - exit
 
-# Test Mode
+# ################## Test Mode ################## #
+
 Options   - toggle program sending packets
 
-# Drive System:
+# ################## Speed Modes ################## #
+
+R3/L3     - toggle through preset list of changes in speed [18, 36, 54, 72, 90, 108, 127]
+L1        - holding enables throttle control (can only use 1 of Arm or Wrist Systems at a time)
+
+# ################## Drive System ################## #
+
 R1        - holding allows for control of drive system
 R Stick   - controls right set of wheels with foward and back inputs
 L Stick   - controls left set of wheels with foward and back inputs
 
-# Arm System:
+# ################## Arm System ################## #
+
 R2        - throttle select motor clockwise
 L2        - throttle select motor counterclockwise
 △        - holding selects gantry (R2 up / L2 down)
@@ -28,14 +39,48 @@ L2        - throttle select motor counterclockwise
 X         - holding selects elbow (R2 up / L2 down)
 □         - holding selects claw (R2 open / L2 close)
 
-# Wrist System
-R3/L3     - toggle through preset speeds (slow, medium, fast)
-L1        - holding enables throttle control (deactivate arm system when on)
-R2        - (when holding L1) throttle select wrist action
+# ################## Wrist System ################## #
 
-(preset or throttle speed) 
+R2        - (when holding L1) throttle select wrist action
 up        - move wrist up
 down      - move wrist down
 left      - spin wrist counterclockwise
 right     - spin wrist clockwise
 
+# ################## PYGAME INPUT MAPPING ################## #
+
+
+# Playstation 4 Controller (pygame 2.x)
+# The PlayStation 4 controller mapping has 6 axes and 16 buttons. The controller is recognized as "PS4 Controller".
+
+# Left Stick:
+# Left -> Right   - Axis 0
+# Up   -> Down    - Axis 1
+# 
+# Right Stick:
+# Left -> Right   - Axis 2
+# Up   -> Down    - Axis 3\
+
+# Left Trigger:
+# Out -> In       - Axis 4
+
+# Right Trigger:
+# Out -> In       - Axis 5
+
+# Buttons:
+# Cross Button    - Button 0
+# Circle Button   - Button 1
+# Square Button   - Button 2
+# Triangle Button - Button 3
+# Share Button    - Button 4
+# PS Button       - Button 5
+# Options Button  - Button 6
+# L. Stick In     - Button 7
+# R. Stick In     - Button 8
+# Left Bumper     - Button 9
+# Right Bumper    - Button 10
+# D-pad Up        - Button 11
+# D-pad Down      - Button 12
+# D-pad Left      - Button 13
+# D-pad Right     - Button 14
+# Touch Pad Click - Button 15
--- app/__main__.py
+++ app/__main__.py
@@ -1,255 +1,142 @@
-
-# Playstation 4 Controller (pygame 2.x)
-# The PlayStation 4 controller mapping has 6 axes and 16 buttons. The controller is recognized as "PS4 Controller".
-
-# Left Stick:
-# Left -> Right   - Axis 0
-# Up   -> Down    - Axis 1
-# 
-# Right Stick:
-# Left -> Right   - Axis 2
-# Up   -> Down    - Axis 3\
-
-# Left Trigger:
-# Out -> In       - Axis 4
-
-# Right Trigger:
-# Out -> In       - Axis 5
-
-# Buttons:
-# Cross Button    - Button 0
-# Circle Button   - Button 1
-# Square Button   - Button 2
-# Triangle Button - Button 3
-# Share Button    - Button 4
-# PS Button       - Button 5
-# Options Button  - Button 6
-# L. Stick In     - Button 7
-# R. Stick In     - Button 8
-# Left Bumper     - Button 9
-# Right Bumper    - Button 10
-# D-pad Up        - Button 11
-# D-pad Down      - Button 12
-# D-pad Left      - Button 13
-# D-pad Right     - Button 14
-# Touch Pad Click - Button 15
-
-
 import socket
 import pygame
+from controller import Controller
+from motors import WheelMotor, ArmMotor, WristMotors
 
 #server address vars
 ADDRESS = 'localhost'
 PORT = '12345'
 
+# Global Speed Setting
+speed_levels = [18, 36, 54, 72, 90, 108, 127]
+change_in_speed_count = 0
+change_in_speed = speed_levels[change_in_speed_count]
+
+def increase_change_in_speed():
+    global change_in_speed_count, change_in_speed
+    if change_in_speed_count < len(speed_levels) - 1:
+        change_in_speed_count += 1
+        change_in_speed = speed_levels[change_in_speed_count]
+        print("Change in Speed: ", change_in_speed)
+
+def decrease_change_in_speed():
+    global change_in_speed_count, change_in_speed
+    if change_in_speed_count > 0:
+        change_in_speed_count -= 1
+        change_in_speed = speed_levels[change_in_speed_count]
+        print("Change in Speed: ", change_in_speed)
+
 def send_controller_input():
-
-    #initialize pygame
-    pygame.init()
-
-    #initailize the controller module
-    pygame.joystick.init()
-
-    #check for controller
-    controller_count = pygame.joystick.get_count()
-    if controller_count == 0:
-        print("CONTROLLER NOT FOUND")
-        pygame.quit
-        quit()
 
     # #open socket (AF_INET, AF_BLUETOOTH, idk) (SOCK_DGRAM because I want a what real-time feedback and packet loss is okay)
     client_socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM)
     server_address = (ADDRESS,PORT)
-    #initalize controller
-    controller = pygame.joystick.Joystick(0)
-    controller.init()
-    print("CONTROLLER HAS BEEN CONNECTED")
+
+    # #initalize controller
+    controller = Controller()
     
-    
+    ##CREATING MOTOR INSTANCES
+    right_wheels = WheelMotor()
+    left_wheels = WheelMotor()
+    gantry = ArmMotor()
+    shoulder = ArmMotor()
+    elbow = ArmMotor()
+    claw = ArmMotor()
+    wrist = WristMotors()
+
     ##Settings vars##
-    
-    STATIONARY = 128
     last_A_command = ''
     last_D_command = ''
-
-    count  = 0
-    slow = 20
-    medium = 64
-    fast = 127
-    speeds = [slow, medium, fast]
-    speed_labels = ["Slow", "Medium", "Fast"]
-    speedMode  = speeds[count]
-    #if left bumper is pressed
-    dpad_throttle_mode = False
+    mode = "set"
 
     #milliseconds delay betwwen loops (50 = 20htz refresh rate)
     MILLISECONDS = 50
 
     #testmode toggles if packets are sent or not
-    testmode = False
+    testmode = True
 
     
-
     #main loop (IMPORTANT THAT THE PACKETS ARE EVENT DRIVEN TO REDUCE REDUNANCY)
     running = True
     while running:
 
-        ##VARIABLES PWM##
-        #WHEELS
-        rightWheel1 = rightWheel2 = rightWheel3 = STATIONARY
-        leftWheel1  = leftWheel2  = leftWheel3  = STATIONARY
-
-        #ARM
-        elbow       = STATIONARY
-        wristright  = STATIONARY
-        wristleft   = STATIONARY
-        claw        = STATIONARY
-        gantry      = STATIONARY
-        shoulder    = STATIONARY
+        ##reset all motors to 128
+        right_wheels.reset()
+        left_wheels.reset()
+        gantry.reset()
+        shoulder.reset()
+        elbow.reset()
+        claw.reset()
+        wrist.reset()
 
         #TRIGGER THROTTLE
         l2Throttle  = (controller.get_axis(4)+1)/2    
         r2Throttle  = (controller.get_axis(5)+1)/2
         total_throttle = r2Throttle - l2Throttle
 
-
-        ##DRIVE FUNCTIONS##
-
-        ##event update wheel motor right side##
-        rightstick = controller.get_axis(3)
-        #check if right bumper for gas
-        if controller.get_button(10) != 0:
-            #giving a lil deadzone to account for stickdrift
-            #forward (CW)(0 to -1)
-            if rightstick < -0.05:
-                speed = STATIONARY - (127 * rightstick)
-                rightWheel1 = rightWheel2 = rightWheel3 = speed
-            #backward (CCW)(0 to 1)
-            elif rightstick > 0.05:
-                speed = STATIONARY - (128 * rightstick)
-                rightWheel1 = rightWheel2 = rightWheel3 = speed
-
-        ##event update wheel motor left side##
-        leftstick = controller.get_axis(1)
-        #check if right bumper for gas
-        if controller.get_button(10) != 0:
-            #giving a lil deadzone to account for stickdrift
-            #forward (CCW)(0 to -1)
-            if leftstick < -0.05:
-                speed = STATIONARY + (128 * leftstick)
-                leftWheel1 = leftWheel2 = leftWheel3 = speed
-            #backward (CW)(0 to 1)
-            elif leftstick > 0.05:
-                speed = STATIONARY + (127 * leftstick)
-                leftWheel1 = leftWheel2 = leftWheel3 = speed
+        ##getting stick pos
+        right_stick = controller.get_axis(3)
+        left_stick = controller.get_axis(1)
 
 
-
-
-
-        ##ARM FUNCTIONS## 
-
-        # Cross Button    - Button 0
-        # Circle Button   - Button 1
-        # Square Button   - Button 2
-        # Triangle Button - Button 3    
-
-        ##event update gantry motor##
-        if controller.get_button(3) == 1 and dpad_throttle_mode == False:
-            if total_throttle > 0:
-                gantry = gantry + (127 * total_throttle)
-            if total_throttle < 0:
-                gantry = gantry + (128 * total_throttle)
-            #print("Gantry: ", gantry)
-
-        ##event update elbow motor##
-        if controller.get_button(0) == 1 and dpad_throttle_mode == False:
-            if total_throttle > 0:
-                elbow = elbow + (127 * total_throttle)
-            if total_throttle < 0:
-                elbow = elbow + (128 * total_throttle)
-            #print("Elbow: ", elbow)
-
-        ##event update shoulder motor##
-        if controller.get_button(1) == 1 and dpad_throttle_mode == False:
-            if total_throttle > 0:
-                shoulder = shoulder + (127 * total_throttle)
-            if total_throttle < 0:
-                shoulder = shoulder + (128 * total_throttle)
-            #print("Shoulder: ", shoulder)
+##DRIVE FUNCTIONS##
+        right_wheels.drive(right_stick, change_in_speed, mode)
+        left_wheels.drive(left_stick, change_in_speed, mode)
         
-        ##event update claw motor##
-        if controller.get_button(2) == 1 and dpad_throttle_mode == False :
-            if total_throttle > 0:
-                claw = claw + (127 * total_throttle)
-            if total_throttle < 0:
-                claw = claw + (128 * total_throttle)
-            #print("claw: ", claw)
 
+# ##ARM FUNCTIONS##
+    
+        #check if using throttle for wrist
+        using_wrist_throttle = (controller.get_button(9) == 1 and (controller.get_button(11) == 1 or controller.get_button(12) == 1 or controller.get_button(13) == 1 or controller.get_button(14) == 1))
+        if not using_wrist_throttle:
 
+            ##event update gantry motor##
+            if controller.get_button(3) != 0:
+                gantry.move(total_throttle, change_in_speed, mode)
 
+            ##event update claw motor##
+            if controller.get_button(2) != 0:
+                claw.move(total_throttle, change_in_speed, mode)
 
+            ##event update shoulder motor##
+            if controller.get_button(1) != 0:
+                shoulder.move(total_throttle, change_in_speed, mode)
 
-        ##wrist functions##
+            ##event update elbow motor##
+            if controller.get_button(0) != 0:
+                elbow.move(total_throttle, change_in_speed, mode)
+        
 
-        #  128 - 255 PWM Wristright and Wristleft -> Spins the claw clockwise
-        #  128 - 255 PWM Wristleft and 128 - 0 PWM Wristright -> claw moves up
-        # D-pad Up        - Button 11
-        # D-pad Down      - Button 12
-        # D-pad Left      - Button 13
-        # D-pad Right     - Button 14
+##wrist functions##
 
-        # move claw upwards
-        if controller.get_button(11) == 1:
-            if dpad_throttle_mode:
-                if r2Throttle > 0:
-                    wristleft = wristleft + (127 * r2Throttle)
-                    wristright = wristright - (128 * r2Throttle)
-            else:
-                wristleft = STATIONARY + speedMode
-                wristright = 127 - speedMode
+        #check if using throttle for arm
+        using_arm_throttle = (controller.get_button(9) == 1 and (controller.get_button(0) == 1 or controller.get_button(1) == 1 or controller.get_button(2) == 1 or controller.get_button(3) == 1))
+        if not using_arm_throttle:
 
-        # move claw downwards
-        if controller.get_button(12) == 1:
-            if dpad_throttle_mode:
-                if r2Throttle > 0:
-                    wristleft = wristleft - (128 * r2Throttle)
-                    wristright = wristright + (127 * r2Throttle)
-            else:
-                wristleft = 127 - speedMode
-                wristright = STATIONARY + speedMode
+            ##event d-pad up
+            if controller.get_button(11) != 0:
+                wrist.move(r2Throttle, change_in_speed, 'up', mode)
+            
+            ##event d-pad down
+            if controller.get_button(12) != 0:
+                wrist.move(r2Throttle, change_in_speed, 'down', mode)
 
-        # spin claw CCW
-        if controller.get_button(13) == 1:
-            if dpad_throttle_mode:
-                if r2Throttle > 0:
-                    wristleft = wristleft - (128 * r2Throttle)
-                    wristright = wristright - (128 * r2Throttle)
-            else:
-                wristleft = 127 - speedMode
-                wristright = 127 - speedMode
+            ##event d-pad left
+            if controller.get_button(13) != 0:
+                wrist.move(r2Throttle, change_in_speed, 'left', mode)
+            
+            ##event d-pad right
+            if controller.get_button(14) != 0:
+                wrist.move(r2Throttle, change_in_speed, 'right', mode)
+        
 
-        # spin claw CW
-        if controller.get_button(14) == 1:
-            if dpad_throttle_mode:
-                if r2Throttle > 0:
-                    wristleft = wristleft + (127 * r2Throttle)
-                    wristright = wristright + (127 * r2Throttle)
-            else:
-                wristleft = STATIONARY + speedMode
-                wristright = STATIONARY + speedMode
-
-
-
-
-
-        ##PACKET ASSEMBLY AND SHIPMENT##
+##PACKET ASSEMBLY AND SHIPMENT##
 
         #“A_elbow_wristright_wristleft_claw_gantry_shoulder”
-        arm_command = f"A_{elbow}_{wristright}_{wristleft}_{claw}_{gantry}_{shoulder}"
+        arm_command = f"A_{elbow.speed}_{wrist.wristright_speed}_{wrist.wristleft_speed}_{claw.speed}_{gantry.speed}_{shoulder.speed}"
 
         #“D_rightWheel1_rightWheel2_rightWheel3_leftWheel1_leftWheel2_leftWheel3”  
-        wheels_command = f"D_{rightWheel1}_{rightWheel2}_{rightWheel3}_{leftWheel1}_{leftWheel2}_{leftWheel3}"
+        wheels_command = f"D_{right_wheels.speed}_{right_wheels.speed}_{right_wheels.speed}_{left_wheels.speed}_{left_wheels.speed}_{left_wheels.speed}"
         
         ##print outgoing commands
         #reduce redundant commands by storing last command
@@ -283,29 +170,32 @@ def send_controller_input():
                 running = False 
 
             if event.type == pygame.JOYBUTTONUP and event.button == 8:
-                count += 1
-                if count > 2:
-                    count = 0
-                speedMode = speeds[count]
-                print(speed_labels[count],": ", speedMode)
+                if change_in_speed ==  127:
+                    print("Max Speed")
+                else:
+                    increase_change_in_speed()
+                    print(change_in_speed)
                 
 
             if event.type == pygame.JOYBUTTONUP and event.button == 7:
-                count -= 1
-                if count <= -1:
-                    count = 2
-                speedMode = speeds[count]
-                print(speed_labels[count],": ", speedMode)
+                if change_in_speed == 18:
+                    print('Min Speed')
+                else:
+                    decrease_change_in_speed()
+                    print(change_in_speed)
 
-                
-
-            if event.type == pygame.JOYBUTTONDOWN and event.button == 9:
-                dpad_throttle_mode = True
-                print("D-PAD THROTTLE MODE : ON")
             
             if event.type == pygame.JOYBUTTONUP and event.button == 9:
-                dpad_throttle_mode = False
-                print("D-PAD THROTTLE MODE : OFF")
+                if mode == "throttle":
+                    mode = 'set'
+                    print("SET SPEED MODE : ON")
+            
+
+            if event.type == pygame.JOYBUTTONDOWN and event.button == 9:
+                if mode == 'set':
+                    mode = 'throttle'
+                    print("THROTTLE SPEED MODE : ON")
+            
 
             if event.type == pygame.JOYBUTTONUP and event.button == 6:
                 if testmode == False:
